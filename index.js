0569dbac4cefe5b8757696d8e3c7afe86293848f
yoav-ro <89576513+yoav-ro@users.noreply.github.com>
2021-11-01 20:32:07 +0200

    port fix

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const pokemonRouter = require("./back/src/routers/pokemonRouter.js")
-const userRouter= require("./back/src/routers/userRouter.js")
-const path= require("path")
-const cors= require("cors")
+const userRouter = require("./back/src/routers/userRouter.js")
+const path = require("path")
+const cors = require("cors")
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(cors())
 app.use("/pokemon", pokemonRouter);
 app.use(userRouter);
 
 app.use('/', express.static(path.resolve('./front'))); // serve main path as static dir
-app.get('/', function(req, res) { // serve main path as static file
+app.get('/', function (req, res) { // serve main path as static file
   res.sendFile(path.resolve('./front/pokedex.html'))
 });
 
